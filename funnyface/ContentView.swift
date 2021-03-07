052a63210c7b37abeadfa62371285110bf736189
Hieu Trac <hieu.trac@nordstrom.com>
2021-03-07 12:56:46 -0800

    Add some sample props. Add eye tracking for eyeball prop. Allow ability to take a snapshot of the screen.

--- funnyface/ContentView.swift
+++ funnyface/ContentView.swift
@@ -5,33 +5,95 @@
 //  Created by Hieu C Trac on 3/6/21.
 //
 
+import ARKit
 import SwiftUI
 import RealityKit
 
 struct ContentView : View {
-    var body: some View {
-        return ARViewContainer().edgesIgnoringSafeArea(.all)
+    
+    @State var currentProp: Prop = .glasses
+    @State var shouldTakeSnapShot: Bool = false
+    
+    var body: some View {        
+        ZStack(alignment: .bottom) {
+            ARViewContainer(currentProp: $currentProp, shouldTakeSnapShot: $shouldTakeSnapShot).edgesIgnoringSafeArea(.all)
+            PropChooser(currentProp: $currentProp, shouldTakeSnapShot: $shouldTakeSnapShot)
+        }
     }
 }
 
 struct ARViewContainer: UIViewRepresentable {
     
-    func makeUIView(context: Context) -> ARView {
+    @Binding var currentProp: Prop
+    @Binding var shouldTakeSnapShot: Bool
+
+    class Coordinator: NSObject, ARSessionDelegate {
+        var arViewContainer: ARViewContainer
+        var arView: ARView!
         
-        let arView = ARView(frame: .zero)
+        init(_ arViewContainer: ARViewContainer) {
+            self.arViewContainer = arViewContainer
+        }
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
+        @objc func image(_ image: UIImage, didFinishSavingWithError error: NSError?, contextInfo: UnsafeRawPointer) {
+            arViewContainer.shouldTakeSnapShot = false
+        }
         
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
+        func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
+            guard let faceAnchor = anchors.first(where: { $0 is ARFaceAnchor }) as? ARFaceAnchor else { return }
+            eyeballLook(at: faceAnchor.lookAtPoint)
+        }
         
-        return arView
-        
+        private func eyeballLook(at point: simd_float3) {
+            guard let eyeball = arView.scene.findEntity(named: "Eyeball") else { return }
+            
+            eyeball.look(at: point, from: eyeball.position, upVector: SIMD3<Float>(0, 1, -1), relativeTo: eyeball.parent)
+        }
+    }
+            
+    func makeCoordinator() -> ARViewContainer.Coordinator {
+        Coordinator(self)
     }
     
-    func updateUIView(_ uiView: ARView, context: Context) {}
-    
+    func makeUIView(context: Context) -> ARView {
+        let arView = ARView(frame: .zero)
+        arView.session.delegate = context.coordinator
+        return arView
+    }
+
+    func updateUIView(_ uiView: ARView, context: Context) {
+        context.coordinator.arView = uiView
+        uiView.scene.anchors.removeAll()
+        
+        let arConfiguration = ARFaceTrackingConfiguration()
+        uiView.session.run(arConfiguration, options: [.resetTracking, .removeExistingAnchors])
+        
+        var anchor: RealityKit.HasAnchoring
+        switch currentProp {
+        case .fancyHat:
+            anchor = try! Experience.loadFancyHat()
+        case .speechBubble:
+            anchor = try! Experience.loadSpeechBubble()
+        case .eyeball:
+            anchor = try! Experience.loadEyeBall()
+        case .glasses:
+            anchor = try! Experience.loadGlasses()
+        case .food:
+            anchor = try! Experience.loadFood()
+        case .mustache:
+            anchor = try! Experience.loadMustache()
+        }
+        
+        uiView.scene.addAnchor(anchor)
+        
+        if (shouldTakeSnapShot) {
+            uiView.snapshot(saveToHDR: false) { (image) in
+                if let pngImage = image?.pngData(), let compressedImage = UIImage(data: pngImage) {
+                    UIImageWriteToSavedPhotosAlbum(compressedImage, context.coordinator, #selector(Coordinator.image(_:didFinishSavingWithError:contextInfo:)), nil)
+                }
+            }
+        }
+    }
 }
 
 #if DEBUG
---
+++ funnyface/PropChooser.swift
@@ -0,0 +1,73 @@
+//
+//  PropChooser.swift
+//  funnyface
+//
+//  Created by Hieu C Trac on 3/6/21.
+//
+
+import SwiftUI
+import RealityKit
+
+enum Prop: CaseIterable, Equatable {
+    case fancyHat, speechBubble, eyeball, glasses, food, mustache
+    
+    private func nextCase(_ cases: [Self]) -> Self? {
+        if self == cases.last {
+            return cases.first
+        } else {
+            return cases
+                .drop(while: ) { $0 != self }
+                .dropFirst()
+                .first
+        }
+    }
+    
+    func next() -> Self {
+        nextCase(Self.allCases) ?? .eyeball
+    }
+    
+    func previous()  -> Self {
+        nextCase(Self.allCases.reversed()) ?? .fancyHat
+    }
+}
+
+struct PropChooser: View {
+    
+    @Binding var currentProp: Prop
+    @Binding var shouldTakeSnapShot: Bool
+    
+    var body: some View {
+        HStack {
+            Button(action: {
+                currentProp = currentProp.previous()
+            }) {
+                Image(systemName: "arrowtriangle.left.fill")
+                    .resizable()
+                    .frame(width: 44, height: 44)
+            }
+            Spacer()
+            Button(action: {
+                shouldTakeSnapShot = true
+            }) {
+                Circle().stroke(lineWidth: 12.0)
+            }
+            Spacer()
+            Button(action: {
+                currentProp = currentProp.next()
+            }) {
+                Image(systemName: "arrowtriangle.right.fill")
+                    .resizable()
+                    .frame(width: 44, height: 44)
+            }
+        }
+        .frame(height: 100)
+        .foregroundColor(.primary)
+        .padding(.horizontal)
+    }
+}
+
+struct PropChooser_Previews: PreviewProvider {
+    static var previews: some View {
+        PropChooser(currentProp: .constant(Prop.fancyHat), shouldTakeSnapShot: .constant(false))
+    }
+}
